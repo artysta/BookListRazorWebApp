66ffbaa0ec81a9f4e98d0817703ca80c62748648
Adrian Kurek <adrian.kurek93@gmail.com>
2020-10-30 22:11:55 +0100

    Add book model

---
+++ BookListRazorWebApp/Model/Book.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookListRazorWebApp.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Name { get; set; }
+        [Required]
+        public int Author { get; set; }
+    }
+}
