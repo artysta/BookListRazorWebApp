db1aee8ea8e45e807f4413705e47d691f4cc14a9
Adrian Kurek <adrian.kurek93@gmail.com>
2020-11-03 19:18:12 +0100

    Add possibility to create new book

---
+++ BookListRazorWebApp/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,39 @@
+using BookListRazorWebApp.Model;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookListRazorWebApp.Pages.BookList
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.Book.AddAsync(Book);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+    }
+}
